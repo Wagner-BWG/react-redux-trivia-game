f6620135af7091366900172415333dd7ee5a2969
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-05-24 15:59:17 -0300

    adiciona requisito 1 ao projeto

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  imageURL: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
