ec1c7cbe07b4b6f6d063baec03488fd5cfba0761
Wagner B <wagner_mbarbosa@hotmail.com>
2022-05-25 14:29:58 -0300

    Adicionado o rootReducer e o gameReducer a ele

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import gameReducer from './game';
+
+import playerReducer from './player';
+
+const rootReducer = combineReducers({
+  player: playerReducer,
+  game: gameReducer,
+});
+
+export default rootReducer;
