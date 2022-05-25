fa84dc878e01a424de6c37340821e2d34076dd13
Wagner B <wagner_mbarbosa@hotmail.com>
2022-05-25 17:56:15 -0300

    Teste não passa

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Quizz from '../components/Quiz';
 
-export default class Game extends Component {
+class Game extends Component {
+  componentDidMount() {
+    const { playerToken, history } = this.props;
+    const { response_code: responseCode } = playerToken;
+    console.log(responseCode);
+    const MAGIC_NUMBER = 3;
+    if (responseCode === MAGIC_NUMBER) {
+      history.push('/');
+    }
+  }
+
   render() {
     return (
       <div>
@@ -11,3 +23,20 @@ export default class Game extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  playerToken: state.player.playerToken,
+});
+
+Game.propTypes = {
+  playerToken: PropTypes.objectOf(PropTypes.any).isRequired,
+  history: PropTypes.shape(),
+  push: PropTypes.func,
+};
+
+Game.defaultProps = {
+  push: () => {},
+  history: {},
+};
+
+export default connect(mapStateToProps)(Game);
--- src/redux/reducers/game.js
+++ src/redux/reducers/game.js
@@ -1,13 +1,12 @@
 import { SET_GAME } from '../actions';
 
-const INITIAL_STATE = {
-  questions: [],
-};
+const INITIAL_STATE = [];
 
 const gameReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SET_GAME:
-    return { ...state, ...action.payload };
+    return [...state, ...action.payload];
+
   default:
     return state;
   }
